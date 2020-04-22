972266c7b4d822ef0151873237c06a8439c4a55d
renatopcinfo <renatopcinfo@gmail.com>
2020-04-22 16:58:33 -0300

    fibonacci

--- src/index.js
+++ src/index.js
@@ -2,26 +2,29 @@
 
 const fibonacci = () => {
     let fibo = [];
-    let element = 0;
+    let numFibo = 0;
 
     for (let i = 0; ; i++) {
         if (i < 2) {
             fibo.push(i);
             continue;
         }
-        element = fibo[i - 1] + fibo[i - 2];
+        numFibo = fibo[i - 1] + fibo[i - 2];
 
-        if (element >= 350) {
+        if (numFibo >= 350) {
             break
         }
 
-        fibo.push(element);
+        fibo.push(numFibo);
     }
 
     return fibo;
 }
+//console.log(fibonacci())
 
 const isFibonnaci = (num) => fibonacci().includes(num);
+
+//console.log(isFibonnaci(13))
 
 module.exports = {
     fibonacci,
